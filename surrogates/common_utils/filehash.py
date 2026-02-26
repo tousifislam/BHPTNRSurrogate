56cef553f3dce4c959ecd1db0a6f9b35e3929776
tousifislam <tousifislam24@gmail.com>
2026-02-26 01:41:32 -0800

    Print download progress to stdout instead of using logger.info
    The logging messages were silently dropped since no handler was
    configured, leaving users with no feedback while large h5 files
    were being downloaded from Zenodo.

--- surrogates/common_utils/filehash.py
+++ surrogates/common_utils/filehash.py
@@ -6,10 +6,7 @@
 
 import os
 import hashlib
-import logging
 import urllib.request
-
-logger = logging.getLogger(__name__)
 
 #----------------------------------------------------------------------------------------------------
 def md5(fname, h5_data_dir, zenodo_ID):
@@ -21,16 +18,15 @@ def md5(fname, h5_data_dir, zenodo_ID):
     # download file if not already there
     if not os.path.isfile(file_path):
         url = 'https://zenodo.org/record/%s/files/%s' % (zenodo_ID, fname)
-        logger.info('%s file is not found in the directory - PATH-TO/BHPTNRSurrogate/data/' % fname)
-        logger.info('... downloading h5 file from zenodo')
-        logger.info('... this might take some time')
+        print('%s not found in BHPTNRSurrogate/data/' % fname)
+        print('Downloading %s from Zenodo ... this may take a few minutes ...' % fname)
         try:
             urllib.request.urlretrieve(url, file_path)
         except Exception as e:
             raise RuntimeError(
                 "Failed to download %s from %s: %s" % (fname, url, e)
             )
-        logger.info('... downloaded')
+        print('Download complete: %s' % fname)
 
     hash_md5 = hashlib.md5()
     with open(file_path, "rb") as f:
